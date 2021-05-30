795ffdb538af2de8b2ad1d053a8473246bf89101
Bartosz Kostrowiecki <bartosz.kostrowiecki@gmail.com>
2021-05-31 01:36:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Polish translation of yup validation messages
+
+This repository contains a single JavaScript file which exports Polish versions for [Yup](https://github.com/jquense/yup) validation messages. 
+
+`mixed.notType` hasn't been translated as it would need a helper method that is not publicly exported by Yup
+
+## Usage
+
+```
+npm install yup-locale-pl
+```
+
+or
+```
+yarn install yup-locale-pl
+```
+
+```javascript
+import * as yup from 'yup';
+import yupLocalePL from 'yup-locale-pl';
+
+yup.setLocale(yupLocalePL);
+```
+
+Make sure to set your localized messages before any other calls to Yup, see [yup#using-a-custom-locale-dictionary](https://github.com/jquense/yup#using-a-custom-locale-dictionary) for details.
---
+++ locale.js
@@ -0,0 +1,59 @@
+export let mixed = {
+  default: 'Pole ${path} jest niepoprawne',
+  required: 'Pole ${path} jest wymagane',
+  oneOf: 'Pole ${path} musi mieć jedną z następujących wartości: ${values}',
+  notOneOf: 'Pole ${path} nie może mieć jednej z następujących wartości: ${values}',
+  defined: '${path} must be defined',
+};
+
+export let string = {
+  length: 'Pole ${path} musi mieć dokładnie ${length} znaków',
+  min: 'Pole ${path} musi mieć conajmniej ${min} znaków',
+  max: 'Pole ${path} może mieć conajwyej ${max} znaków',
+  matches: 'Pole ${path} musi pasować do następującego wzorca: "${regex}"',
+  email: 'Pole ${path} musi być poprawnym adresem email',
+  url: 'Pole ${path} musi być poprawnym adresem URL',
+  uuid: 'Pole ${path} musi być poprawnym identyfikatorem UUID',
+  trim: 'Pole ${path} musi być tekstem bez spacji na początku i na końcu',
+  lowercase: 'Pole ${path} może mieć tylko małe litery',
+  uppercase: 'Pole ${path} może mieć tylko wielkie litery',
+};
+
+export let number = {
+  min: 'Pole ${path} musi być liczbą większą lub równą ${min}',
+  max: 'Pole ${path} musi być liczą mniejsa lub równą ${max}',
+  lessThan: 'Pole ${path} musi być liczbą mniejszą od ${less}',
+  moreThan: 'Pole ${path} musi być liczbą więszką od ${more}',
+  positive: 'Pole ${path} musi być liczbą dodatnią',
+  negative: 'Pole ${path} musi być liczbą ujemną',
+  integer: 'Pole ${path} musi być liczbą całkowitą',
+};
+
+export let date = {
+  min: 'Pole ${path} musi zawierać datę późniejszą niż ${min}',
+  max: 'Pole ${path} musi zawierać datę wcześniejszą niż ${max}',
+};
+
+export let boolean = {
+  isValue: 'Pole ${path} musi być ${value}',
+};
+
+export let object = {
+  noUnknown: 'Pole ${path} zawiera nieznane klucze: ${unknown}',
+};
+
+export let array = {
+  min: 'Pole ${path} musi zawierać conajmniej ${min} elementów',
+  max: 'Pole ${path} może zawierać conajwyżej ${max} elementów',
+  length: 'Pole ${path} musi mieć dokładnie ${length} elementów',
+};
+
+export default Object.assign(Object.create(null), {
+  mixed,
+  string,
+  number,
+  date,
+  object,
+  array,
+  boolean,
+});
